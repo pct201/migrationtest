cd560e520d56bdfff3141b0c07a1d97178cc8728
nihir.m <nihir.m@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2015-07-10 10:07:57 +0000

    3318
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@63424 8092c8c6-7ab6-f546-91b1-2980cf571e14

--- eRims_SonicWeb/JavaScript/scroll.js
+++ eRims_SonicWeb/JavaScript/scroll.js
@@ -5,27 +5,52 @@
 (function () {
     //for non IE modern browser
     if (window.addEventListener) {
-        function loadFn () {
-            var currentX = window.pageXOffset, currentY = window.pageYOffset,
-            screenWidth =  window.innerWidth, screenHeight = window.innerHeight,
-            scrollMaxX = window.scrollMaxX, scrollMaxY = window.scrollMaxY,
-            winMaxHeight = scrollMaxY + screenHeight, winMaxWidth = scrollMaxX + screenWidth, x, y;
-            if (scrollMaxX > 0 || scrollMaxY > 0) {
-                y = 0;
-                do{
-                    x = 0;
-                    do {
-                        window.scrollTo(x, y);
-                        x += screenWidth;
-                    } while (x < winMaxWidth)
-                    y += screenHeight;
-                } while(y < winMaxHeight)
+        //Function to Scroll all page area by default.
+        function pageScrollArea() {
+            var body = document.body, element = document.documentElement,
+                eleScrollWidth = 0, eleOffsetWidth = 0, bodyScrollWidth = 0, bodyOffsetWidth = 0,
+                eleScrollHeight = 0, eleOffsetHeight = 0, bodyScrollHeight = 0, bodyOffsetHeight = 0,
+                pageWidth = 0, pageHeight = 0, viewWidth = 0, viewHeight = 0,
+                currentTop = 0, currentLeft = 0, scrollWidth = 0, scrollHeight = 0;
+
+            if (element) {
+                eleScrollWidth = element.scrollWidth;
+                eleOffsetWidth = element.offsetWidth;
+                eleScrollHeight = element.scrollHeight;
+                eleOffsetHeight = element.offsetHeight;
+            }
+            if (body) {
+                bodyScrollWidth = body.scrollWidth;
+                bodyOffsetWidth = body.offsetWidth;
+                bodyScrollHeight = body.scrollHeight;
+                bodyOffsetHeight = body.offsetHeight;
             }
 
-            //scroll to current position
-            window.scrollTo(currentX, currentY);
+            // Page actual width and height
+            pageWidth = Math.max(eleScrollWidth, eleOffsetWidth, bodyScrollWidth, bodyOffsetWidth);
+            pageHeight = Math.max(eleScrollHeight, eleOffsetHeight, bodyScrollHeight, bodyOffsetHeight);
+
+            // Page view area width and height
+            viewWidth = window.innerWidth || body.clientWidth || element.clientWidth;
+            viewHeight = window.innerHeight || body.clientHeight || element.clientHeight;
+
+            // Current position of the page
+            currentTop = (body.scrollTop || element.scrollTop);
+            currentLeft = (body.scrollLeft || element.scrollLeft);
+
+
+            // Consequent loop through Width and Height
+            for (scrollWidth = 0; scrollWidth < pageWidth; scrollWidth += viewWidth) {
+                scrollTo(scrollWidth, 0);
+                for (scrollHeight = 0; scrollHeight < pageHeight; scrollHeight += viewHeight) {
+                    scrollBy(0, viewHeight);
+                }
+            }
+
+            // reset the original position of the page
+            scrollTo(currentLeft, currentTop);
         }
-        window.addEventListener('load', loadFn, false);
+        window.addEventListener('load', pageScrollArea, false);
     }
 }())
 
