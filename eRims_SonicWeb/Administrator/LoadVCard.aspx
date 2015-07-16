e5918d7ad3e7a43a27f2372adf04a712c24b2963
vishalp.v <vishalp.v@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2015-07-16 11:36:54 +0000

    3283
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@63687 8092c8c6-7ab6-f546-91b1-2980cf571e14

---
+++ eRims_SonicWeb/Administrator/LoadVCard.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="LoadVCard.aspx.cs" Inherits="Administrator_LoadVCard" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Load VCard</title>
+    <style type="text/css">
+        .box {
+            width: 500px;
+            height: 200px;
+            position: fixed;
+            margin-left: -150px; /* half of width */
+            margin-top: -150px; /* half of height */
+            top: 50%;
+            left: 30%;
+        }
+    </style>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <script type="text/javascript">
+            var validFilesTypes = ["vcf"];
+
+            function CheckExtension(file) {
+                /*global document: false */
+                var filePath = file.value;
+                var ext = filePath.substring(filePath.lastIndexOf('.') + 1).toLowerCase();
+                var isValidFile = false;
+
+                for (var i = 0; i < validFilesTypes.length; i++) {
+                    if (ext == validFilesTypes[i]) {
+                        isValidFile = true;
+                        break;
+                    }
+                }
+
+                if (!isValidFile) {
+                    file.value = null;
+                    alert("Invalid File. Please select VCARD(.vcf) file.");
+                }
+
+                return isValidFile;
+            }
+
+            function valueSave() {
+                var retVal = true;
+                retVal = confirm("Would you like to update the existing Contractor?");
+                if (retVal == true)
+                    __doPostBack(document.getElementById('<%=btnUpload.ClientID%>').name, "UpdateDetails");
+            else {
+                alert('Import Cancelled');
+
+                self.close();
+            }
+
+        }
+    </script>
+        <table width="100%" class="box">
+            <tr>
+                <td>Please select VCard
+                </td>
+                <td>
+                    <asp:FileUpload ID="fluVcard" runat="server" onchange="return CheckExtension(this);" />
+                </td>
+                <td>
+                    <asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="btnUpload_Click" />
+                </td>
+            </tr>
+        </table>
+
+    </form>
+</body>
+</html>
---
+++ eRims_SonicWeb/Administrator/LoadVCard.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Thought.vCards;
+using ERIMS.DAL;
+
+public partial class Administrator_LoadVCard : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ClientScript.GetPostBackEventReference(this, string.Empty);
+
+    }
+    protected void btnUpload_Click(object sender, EventArgs e)
+    {
+        string filePath = Path.Combine(Server.MapPath(".."), "temp");
+        string ActionType = "C";//C = Check, U = Update;
+        string strArgs = (this.Request["__EVENTARGUMENT"] == null) ? string.Empty : this.Request["__EVENTARGUMENT"];
+        if (strArgs != "UpdateDetails")
+        {
+            filePath += "\\" + fluVcard.FileName;
+            fluVcard.SaveAs(filePath);
+            Session["vCardfilePath"] = filePath;
+        }
+        else
+        {
+            filePath = Convert.ToString(Session["vCardfilePath"]);
+            ActionType = "U";
+            Session["vCardfilePath"] = null;
+        }
+        vCard vCard_temp = new vCard(filePath);
+
+        if (vCard_temp != null && !string.IsNullOrEmpty(vCard_temp.FamilyName) && !string.IsNullOrEmpty(vCard_temp.GivenName))
+        {
+            Contractor_Security objContracor = new Contractor_Security();
+            objContracor.First_Name = vCard_temp.GivenName;
+            objContracor.Last_Name = vCard_temp.FamilyName;
+            objContracor.Contractor_Company_Name = vCard_temp.Organization;
+            if (vCard_temp.DeliveryAddresses != null && vCard_temp.DeliveryAddresses.Count > 0)
+            {
+                foreach (vCardDeliveryAddress add in vCard_temp.DeliveryAddresses)
+                {
+                    if (add.IsWork)
+                    {
+                        objContracor.Address_1 = add.Street;
+                        objContracor.City = add.City;
+                        objContracor.State = add.Region;
+                        objContracor.Zip_Code = add.PostalCode;
+                        break;
+                    }
+                }
+            }
+            if (vCard_temp.Phones != null && vCard_temp.Phones.Count > 0)
+            {
+                foreach (vCardPhone phone in vCard_temp.Phones)
+                {
+                    if (phone.IsCellular)
+                        objContracor.Cell_Telephone = phone.FullNumber;
+                    if (phone.IsWork)
+                        objContracor.Office_Telephone = phone.FullNumber;
+                    if (phone.IsPager)
+                        objContracor.Pager = phone.FullNumber;
+                }
+            }
+            if (vCard_temp.EmailAddresses != null && vCard_temp.EmailAddresses.Count > 0)
+            {
+                objContracor.Email = vCard_temp.EmailAddresses[0].Address;
+            }
+            if (Session["PK_Contactor_Security"] != null)
+            {
+                objContracor.PK_Contactor_Security = Convert.ToDecimal(Session["PK_Contactor_Security"]);
+                Session["PK_Contactor_Security"] = null;
+            }
+            string returnVal = objContracor.ImportContractorSecurityFromVCard(ActionType);
+            if (returnVal.ToLower() == "exist")
+            {
+                ScriptManager.RegisterStartupScript(this, typeof(string), DateTime.Now.ToString(), "javascript:valueSave();", true);
+                Session["PK_Contactor_Security"] = objContracor.PK_Contactor_Security;
+                return;
+            }
+            else
+            {
+                string script = "alert('Import successfull.');self.close();";
+                ScriptManager.RegisterClientScriptBlock(Page, this.GetType(), "", script, true);
+            }
+
+        }
+
+    }
+}
