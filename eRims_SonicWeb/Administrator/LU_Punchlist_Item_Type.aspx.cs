68713aaf72ef0b92d7e0351b3546e08629cac425
nihir.m <nihir.m@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2015-07-20 13:11:19 +0000

    3283
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@63824 8092c8c6-7ab6-f546-91b1-2980cf571e14

---
+++ eRims_SonicWeb/Administrator/LU_Punchlist_Item_Type.aspx.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using ERIMS.DAL;
+
+public partial class Administrator_LU_Punchlist_Item_Type : clsBasePage
+{
+    #region "Properties"
+
+    /// <summary>
+    /// Denotes the Primary Key
+    /// </summary>
+    public decimal _PK_LU_Punchlist_Item_Type
+    {
+        get
+        {
+            return clsGeneral.GetInt(ViewState["PK_LU_Punchlist_Item_Type"]);
+        }
+        set { ViewState["PK_LU_Punchlist_Item_Type"] = value; }
+    }
+
+    #endregion
+
+    #region "Page Event"
+    /// <summary>
+    /// Page Load Event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //check Is Page Post Back
+        if (!IsPostBack)
+        {
+            //Bind Grid Function
+            BindGrid();
+        }
+    }
+
+    #endregion
+
+    #region "Event"
+
+    /// <summary>
+    /// Add new button link event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void lnkAddNew_Click(object sender, EventArgs e)
+    {
+        btnAdd.Text = "Add";
+        _PK_LU_Punchlist_Item_Type = 0;
+        trStatusAdd.Style.Add("display", "block");
+        lnkCancel.Style.Add("display", "inline");
+        rdblActive.SelectedValue = "Y";
+        txtType.Text = "";
+        ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtType);
+    }
+
+    /// <summary>
+    /// Cancel button click event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void lnkCancel_Click(object sender, EventArgs e)
+    {
+        trStatusAdd.Style.Add("display", "none");
+        lnkCancel.Style.Add("display", "none");
+        txtType.Text = "";
+        rdblActive.SelectedValue = "Y";
+    }
+
+    /// <summary>
+    /// Add new button to save data
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        decimal _retVal;
+        LU_Punchlist_Item_Type objLU_Punchlist_Item_Type = new LU_Punchlist_Item_Type();
+        objLU_Punchlist_Item_Type.Type = txtType.Text.Trim();
+        objLU_Punchlist_Item_Type.PK_LU_Punchlist_Item_Type = _PK_LU_Punchlist_Item_Type;
+        objLU_Punchlist_Item_Type.Active = rdblActive.SelectedValue;
+
+        if (_PK_LU_Punchlist_Item_Type > 0)
+        {
+            _retVal = objLU_Punchlist_Item_Type.Update();
+            // Used to Check Duplicate Media ID?
+            if (_retVal == -2)
+            {
+                lblError.Text = "The Type that you are trying to add already exists in the Equipment Type table.";
+                ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtType);
+                return;
+            }
+            btnAdd.Text = "Add";
+        }
+        else
+        {
+            _retVal = objLU_Punchlist_Item_Type.Insert();
+            // Used to Check Duplicate Media ID?
+            if (_retVal == -2)
+            {
+                lblError.Text = "The Type that you are trying to add already exists in the Equipment Type table.";
+                ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtType);
+                return;
+            }
+            _PK_LU_Punchlist_Item_Type = _retVal;
+        }
+        //claer Control
+        ClearControls();
+        //Bind Grid Function
+        BindGrid();
+        //Cancel CLick
+        lnkCancel_Click(null, null);
+    }
+
+    #endregion
+
+    #region "Grid Event"
+    /// <summary>
+    /// Page Index Change Event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void gvLU_Punchlist_Item_Type_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvLU_Punchlist_Item_Type.PageIndex = e.NewPageIndex; //Page new index call
+        BindGrid();
+    }
+    /// <summary>
+    /// Row Command Event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void gvLU_Punchlist_Item_Type_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "EditRecord")
+        {
+            _PK_LU_Punchlist_Item_Type = Convert.ToDecimal(e.CommandArgument);
+            // show and hide Add-edit row
+            trStatusAdd.Style.Add("display", "block");
+            lnkCancel.Style.Add("display", "inline");
+            btnAdd.Text = "Update";
+            // get record from database
+
+            LU_Punchlist_Item_Type objLU_Punchlist_Item_Type = new LU_Punchlist_Item_Type(_PK_LU_Punchlist_Item_Type);
+            txtType.Text = objLU_Punchlist_Item_Type.Type.ToString();
+            if (objLU_Punchlist_Item_Type.Active != null)
+                rdblActive.SelectedValue = objLU_Punchlist_Item_Type.Active;
+            else
+                rdblActive.SelectedIndex = 0;
+            ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtType);
+        }
+    }
+
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Bind CRM Department Grid
+    /// </summary>
+    private void BindGrid()
+    {
+        //Get Record into Dataset
+        DataSet dsLU_Punchlist_Item_Type = LU_Punchlist_Item_Type.SelectAll();
+        //Apply Dataset to Grid
+        gvLU_Punchlist_Item_Type.DataSource = dsLU_Punchlist_Item_Type;
+        gvLU_Punchlist_Item_Type.DataBind();
+    }
+
+    /// Used to Claer the controls
+    /// </summary>
+    private void ClearControls()
+    {
+        //clear control
+        _PK_LU_Punchlist_Item_Type = 0;
+    }
+
+    #endregion
+}
---
+++ eRims_SonicWeb/App_Code/SONIC_DAL/LU_Punchlist_Item_Type.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+
+namespace ERIMS.DAL
+{
+	/// <summary>
+	/// Data access class for table LU_Punchlist_Item_Type
+	/// </summary>
+	public sealed class LU_Punchlist_Item_Type
+	{
+
+		#region Private variables used to hold the property values
+
+		private decimal? _PK_LU_Punchlist_Item_Type;
+		private string _Type;
+		private string _Active;
+
+		#endregion
+
+		#region Public Property
+
+		/// <summary>
+		/// Gets or sets the PK_LU_Punchlist_Item_Type value.
+		/// </summary>
+		public decimal? PK_LU_Punchlist_Item_Type
+		{
+			get { return _PK_LU_Punchlist_Item_Type; }
+			set { _PK_LU_Punchlist_Item_Type = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the Type value.
+		/// </summary>
+		public string Type
+		{
+			get { return _Type; }
+			set { _Type = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the Active value.
+		/// </summary>
+		public string Active
+		{
+			get { return _Active; }
+			set { _Active = value; }
+		}
+
+
+		#endregion
+
+		#region Default Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the LU_Punchlist_Item_Type class with default value.
+		/// </summary>
+		public LU_Punchlist_Item_Type() 
+		{
+
+
+		}
+
+		#endregion
+
+		#region Primary Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the LU_Punchlist_Item_Type class based on Primary Key.
+		/// </summary>
+		public LU_Punchlist_Item_Type(decimal pK_LU_Punchlist_Item_Type) 
+		{
+			DataTable dtLU_Punchlist_Item_Type = Select(pK_LU_Punchlist_Item_Type).Tables[0];
+
+			if (dtLU_Punchlist_Item_Type.Rows.Count == 1)
+			{
+				 SetValue(dtLU_Punchlist_Item_Type.Rows[0]);
+
+			}
+
+		}
+
+
+		/// <summary>
+		/// Initializes a new instance of the LU_Punchlist_Item_Type class based on Datarow passed.
+		/// </summary>
+		private void SetValue (DataRow drLU_Punchlist_Item_Type) 
+		{
+				if (drLU_Punchlist_Item_Type["PK_LU_Punchlist_Item_Type"] == DBNull.Value)
+					this._PK_LU_Punchlist_Item_Type = null;
+				else
+					this._PK_LU_Punchlist_Item_Type = (decimal?)drLU_Punchlist_Item_Type["PK_LU_Punchlist_Item_Type"];
+
+				if (drLU_Punchlist_Item_Type["Type"] == DBNull.Value)
+					this._Type = null;
+				else
+					this._Type = (string)drLU_Punchlist_Item_Type["Type"];
+
+				if (drLU_Punchlist_Item_Type["Active"] == DBNull.Value)
+					this._Active = null;
+				else
+					this._Active = (string)drLU_Punchlist_Item_Type["Active"];
+
+
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Inserts a record into the LU_Punchlist_Item_Type table.
+		/// </summary>
+		/// <returns></returns>
+		public int Insert()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Punchlist_Item_TypeInsert");
+
+			
+			if (string.IsNullOrEmpty(this._Type))
+				db.AddInParameter(dbCommand, "Type", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Type", DbType.String, this._Type);
+			
+			if (string.IsNullOrEmpty(this._Active))
+				db.AddInParameter(dbCommand, "Active", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Active", DbType.String, this._Active);
+
+			// Execute the query and return the new identity value
+			int returnValue = Convert.ToInt32(db.ExecuteScalar(dbCommand));
+
+			return returnValue;
+		}
+
+		/// <summary>
+		/// Selects a single record from the LU_Punchlist_Item_Type table.
+		/// </summary>
+		/// <returns>DataSet</returns>
+		private DataSet Select(decimal pK_LU_Punchlist_Item_Type)
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Punchlist_Item_TypeSelect");
+
+			db.AddInParameter(dbCommand, "PK_LU_Punchlist_Item_Type", DbType.Decimal, pK_LU_Punchlist_Item_Type);
+
+			return db.ExecuteDataSet(dbCommand);
+		}
+
+		/// <summary>
+		/// Selects all records from the LU_Punchlist_Item_Type table.
+		/// </summary>
+		/// <returns>DataSet</returns>
+		public static DataSet SelectAll()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Punchlist_Item_TypeSelectAll");
+
+			return db.ExecuteDataSet(dbCommand);
+		}
+
+		/// <summary>
+		/// Updates a record in the LU_Punchlist_Item_Type table.
+		/// </summary>
+		public decimal Update()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Punchlist_Item_TypeUpdate");
+
+			
+			db.AddInParameter(dbCommand, "PK_LU_Punchlist_Item_Type", DbType.Decimal, this._PK_LU_Punchlist_Item_Type);
+			
+			if (string.IsNullOrEmpty(this._Type))
+				db.AddInParameter(dbCommand, "Type", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Type", DbType.String, this._Type);
+			
+			if (string.IsNullOrEmpty(this._Active))
+				db.AddInParameter(dbCommand, "Active", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Active", DbType.String, this._Active);
+
+            return Convert.ToDecimal(db.ExecuteScalar(dbCommand));
+		}
+
+		/// <summary>
+		/// Deletes a record from the LU_Punchlist_Item_Type table by a composite primary key.
+		/// </summary>
+		public static void Delete(decimal pK_LU_Punchlist_Item_Type)
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Punchlist_Item_TypeDelete");
+
+			db.AddInParameter(dbCommand, "PK_LU_Punchlist_Item_Type", DbType.Decimal, pK_LU_Punchlist_Item_Type);
+
+			db.ExecuteNonQuery(dbCommand);
+		}
+	}
+}
