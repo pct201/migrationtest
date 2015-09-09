7c4d01838b0cecb73f8b600b2ec7b3f05e35f62d
hitesh.d <hitesh.d@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2015-09-09 11:09:17 +0000

    3363
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@66386 8092c8c6-7ab6-f546-91b1-2980cf571e14

---
+++ eRims_SonicWeb/Administrator/LU_Contract_Type.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ERIMS.DAL;
+using System.Data;
+
+public partial class Administrator_LU_Contract_Type : clsBasePage
+{
+    #region "Properties"
+    /// <summary>
+    /// Denotes the Primary Key
+    /// </summary>
+    public decimal PK_LU_Contract_Type
+    {
+        get
+        {
+            return clsGeneral.GetInt(ViewState["PK_LU_Contract_Type"]);
+        }
+        set { ViewState["PK_LU_Contract_Type"] = value; }
+    }
+
+    #endregion
+
+    #region "Page Event"
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //Bind Grid Function
+            BindGrid();
+        }
+    }
+    #endregion
+
+    #region "Event"
+
+    /// <summary>
+    /// Add new button link event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void lnkAddNew_Click(object sender, EventArgs e)
+    {
+        btnAdd.Text = "Add";
+        PK_LU_Contract_Type = 0;
+        trStatusAdd.Style.Add("display", "");
+        lnkCancel.Style.Add("display", "");
+        txtContractType.Text = "";
+        
+        ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtContractType);
+    }
+
+    /// <summary>
+    /// Cancel button click event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void lnkCancel_Click(object sender, EventArgs e)
+    {
+        trStatusAdd.Style.Add("display", "none");
+        lnkCancel.Style.Add("display", "none");
+        txtContractType.Text = "";
+        
+    }
+
+    /// <summary>
+    /// Add new button to save data
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        decimal _retVal;
+        LU_Contract_Type objLU_Contract_Type = new LU_Contract_Type();
+        objLU_Contract_Type.Descr = txtContractType.Text.Trim();
+        objLU_Contract_Type.PK_LU_Contract_Type = PK_LU_Contract_Type;
+        
+
+        if (PK_LU_Contract_Type > 0)
+        {
+            _retVal = objLU_Contract_Type.Update();
+            // Used to Check Duplicate CRM Brand ID?
+            if (_retVal == -2)
+            {
+                lblError.Text = "The Contract Type that you are trying to add already exists.";
+                ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtContractType);
+                return;
+            }
+            btnAdd.Text = "Add";
+        }
+        else
+        {
+            _retVal = objLU_Contract_Type.Insert();
+            // Used to Check Duplicate CRM Brand ID?
+            if (_retVal == -2)
+            {
+                lblError.Text = "The Contract Type that you are trying to add already exists.";
+                ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtContractType);
+                return;
+            }
+            PK_LU_Contract_Type = _retVal;
+        }
+        //claer Control
+        ClearControls();
+        //Bind Grid Function
+        BindGrid();
+        //Cancel CLick
+        lnkCancel_Click(null, null);
+    }
+
+    #endregion
+
+    #region "Grid Event"
+
+    protected void gvContract_Type_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvContract_Type.PageIndex = e.NewPageIndex; //Page new index call
+        BindGrid();
+    }
+    protected void gvContract_Type_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "EditRecord")
+        {
+            PK_LU_Contract_Type = Convert.ToDecimal(e.CommandArgument);
+            // show and hide Add-edit row
+            trStatusAdd.Style.Add("display", "");
+            lnkCancel.Style.Add("display", "inline");
+            btnAdd.Text = "Update";
+            // get record from database
+            LU_Contract_Type objLU_Contract_Type = new LU_Contract_Type(PK_LU_Contract_Type);
+            txtContractType.Text = Convert.ToString(objLU_Contract_Type.Descr);
+            
+            ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtContractType);
+        }
+    }
+
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Bind Contract Type Grid
+    /// </summary>
+    private void BindGrid()
+    {
+        //Get Record into Dataset
+        DataSet dsGroup = LU_Contract_Type.SelectAll();
+        //Apply Dataset to Grid
+        gvContract_Type.DataSource = dsGroup;
+        gvContract_Type.DataBind();
+    }
+
+    /// Used to Claer the controls
+    /// </summary>
+    private void ClearControls()
+    {
+        //clear control
+        PK_LU_Contract_Type = 0;
+    }
+
+    #endregion
+}
---
+++ eRims_SonicWeb/App_Code/SONIC_DAL/LU_Contract_Type.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+
+namespace ERIMS.DAL
+{
+	/// <summary>
+	/// Data access class for table LU_Contract_Type
+	/// </summary>
+	public sealed class LU_Contract_Type
+	{
+
+		#region Private variables used to hold the property values
+
+		private decimal? _PK_LU_Contract_Type;
+		private string _Descr;
+
+		#endregion
+
+		#region Public Property
+
+		/// <summary>
+		/// Gets or sets the PK_LU_Contract_Type value.
+		/// </summary>
+		public decimal? PK_LU_Contract_Type
+		{
+			get { return _PK_LU_Contract_Type; }
+			set { _PK_LU_Contract_Type = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the Descr value.
+		/// </summary>
+		public string Descr
+		{
+			get { return _Descr; }
+			set { _Descr = value; }
+		}
+
+
+		#endregion
+
+		#region Default Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the LU_Contract_Type class with default value.
+		/// </summary>
+		public LU_Contract_Type() 
+		{
+
+
+		}
+
+		#endregion
+
+		#region Primary Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the LU_Contract_Type class based on Primary Key.
+		/// </summary>
+		public LU_Contract_Type(decimal pK_LU_Contract_Type) 
+		{
+			DataTable dtLU_Contract_Type = Select(pK_LU_Contract_Type).Tables[0];
+
+			if (dtLU_Contract_Type.Rows.Count == 1)
+			{
+				 SetValue(dtLU_Contract_Type.Rows[0]);
+
+			}
+
+		}
+
+
+		/// <summary>
+		/// Initializes a new instance of the LU_Contract_Type class based on Datarow passed.
+		/// </summary>
+		private void SetValue (DataRow drLU_Contract_Type) 
+		{
+				if (drLU_Contract_Type["PK_LU_Contract_Type"] == DBNull.Value)
+					this._PK_LU_Contract_Type = null;
+				else
+					this._PK_LU_Contract_Type = (decimal?)drLU_Contract_Type["PK_LU_Contract_Type"];
+
+				if (drLU_Contract_Type["Descr"] == DBNull.Value)
+					this._Descr = null;
+				else
+					this._Descr = (string)drLU_Contract_Type["Descr"];
+
+
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Inserts a record into the LU_Contract_Type table.
+		/// </summary>
+		/// <returns></returns>
+		public int Insert()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Contract_TypeInsert");
+
+			
+			if (string.IsNullOrEmpty(this._Descr))
+				db.AddInParameter(dbCommand, "Descr", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Descr", DbType.String, this._Descr);
+
+			// Execute the query and return the new identity value
+			int returnValue = Convert.ToInt32(db.ExecuteScalar(dbCommand));
+
+			return returnValue;
+		}
+
+		/// <summary>
+		/// Selects a single record from the LU_Contract_Type table.
+		/// </summary>
+		/// <returns>DataSet</returns>
+		private DataSet Select(decimal pK_LU_Contract_Type)
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Contract_TypeSelect");
+
+			db.AddInParameter(dbCommand, "PK_LU_Contract_Type", DbType.Decimal, pK_LU_Contract_Type);
+
+			return db.ExecuteDataSet(dbCommand);
+		}
+
+		/// <summary>
+		/// Selects all records from the LU_Contract_Type table.
+		/// </summary>
+		/// <returns>DataSet</returns>
+		public static DataSet SelectAll()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Contract_TypeSelectAll");
+
+			return db.ExecuteDataSet(dbCommand);
+		}
+
+		/// <summary>
+		/// Updates a record in the LU_Contract_Type table.
+		/// </summary>
+        public decimal Update()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Contract_TypeUpdate");
+
+			
+			db.AddInParameter(dbCommand, "PK_LU_Contract_Type", DbType.Decimal, this._PK_LU_Contract_Type);
+			
+			if (string.IsNullOrEmpty(this._Descr))
+				db.AddInParameter(dbCommand, "Descr", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Descr", DbType.String, this._Descr);
+
+            return (Convert.ToDecimal(db.ExecuteScalar(dbCommand)));
+		}
+
+		/// <summary>
+		/// Deletes a record from the LU_Contract_Type table by a composite primary key.
+		/// </summary>
+		public static void Delete(decimal pK_LU_Contract_Type)
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Contract_TypeDelete");
+
+			db.AddInParameter(dbCommand, "PK_LU_Contract_Type", DbType.Decimal, pK_LU_Contract_Type);
+
+			db.ExecuteNonQuery(dbCommand);
+		}
+	}
+}
