2128f1afb9052af5346174a6c7c4d6febcbe726f
dipen.b <dipen.b@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2015-09-29 06:59:22 +0000

    3370
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@67994 8092c8c6-7ab6-f546-91b1-2980cf571e14

--- eRims_SonicWeb/Management/EmailAttachment.aspx.cs
+++ eRims_SonicWeb/Management/EmailAttachment.aspx.cs
@@ -16,7 +16,7 @@ using System.Text;
 public partial class SONIC_EmailAttachment : System.Web.UI.Page
 {
     //string strBody = "This e-mail communication is to request your decision to approve/not approve the settlement on the eRIMS2 Management {Origin_Claim_Number}. Please login to eRIMS2, search for Management {Origin_Claim_Number}, click on the Risk Management Worksheet link on the left side of the screen, scroll down to the Settlement Approvals band and provide your decision and enter the date of approval in the appropriate approval area. Thank you!";
-    string strBody = "This e-mail communication is to request your decision to approve/not approve the settlement on the eRIMS2 ACI Management. Please login to eRIMS2 (Your Sonic Shared Drive – My Business APPS – eRIMS2 – enter your User ID and Password, search for ACI and then ACI Management , click on the Approvals link on the left side of the screen, scroll down to the Approvals band and provide your decision and enter the date of approval in the appropriate approval area. Thank you!";
+    string strBody = "This e-mail communication is to request your decision to approve/not approve the settlement on the eRIMS2 ACI Management. Please login to eRIMS2 (Your Sonic Shared Drive – My Business APPS – eRIMS2 – enter your User ID and Password, search for ACI and then ACI Management , click on the Approvals link on the left side of the screen, scroll down &lt;click&gt; \"Edit\" to the Approvals band and provide your decision and enter the date of approval in the appropriate approval area. Thank you!";
 
     /// <summary>
     /// Page Load Event
@@ -45,8 +45,10 @@ public partial class SONIC_EmailAttachment : System.Web.UI.Page
             //    strManagement_Numberr = Convert.ToString(drManagement["Management_Number"]);
             //}
             decimal PK_Management = clsGeneral.GetDecimal(Request.QueryString["ManagementId"]);
-            string strReference_Number = new clsManagement((decimal)PK_Management).Reference_Number;
-            txtBody.Text = "Reference Number : " + Convert.ToString(strReference_Number) + " \n \n" + strBody.Replace("{Origin_Claim_Number}", string.Empty);//strManagement_Numberr.ToString().Trim()
+            clsManagement management = new clsManagement((decimal)PK_Management);
+            string strReference_Number = management.Reference_Number;
+            string dba = management.FK_LU_Location.HasValue ? new LU_Location(management.FK_LU_Location.Value).dba : "";
+            txtBody.Text = "Store Location Name : " + dba + " \nLocater Number : " + Convert.ToString(strReference_Number) + " \n \n" + strBody.Replace("{Origin_Claim_Number}", string.Empty);//strManagement_Numberr.ToString().Trim()
         }
 
     }
