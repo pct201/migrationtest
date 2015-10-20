615e34c460942e1466f844b0847b1c7739f9472a
dipen.b <dipen.b@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2015-10-20 09:57:25 +0000

    3407
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@69975 8092c8c6-7ab6-f546-91b1-2980cf571e14

--- eRims_SonicWeb/App_Code/ComboHelper.cs
+++ eRims_SonicWeb/App_Code/ComboHelper.cs
@@ -5823,26 +5823,26 @@ public class ComboHelper
     /// </summary>
     /// <param name="dropDownList">All dropDownList</param>
     /// <param name="booladdSelectAsFirstElement">Add --select-- to first item in each drop down or not</param>
-    //public static void FillPaintCategory(DropDownList[] dropDownList, bool booladdSelectAsFirstElement)
-    //{
-    //    DataTable dtData = ERIMS.DAL.clsLU_VOC_Category.SelectAll().Tables[0];
-    //    dtData.DefaultView.RowFilter = " Active = 'Y' ";
-    //    dtData = dtData.DefaultView.ToTable();
+    public static void FillPaintCategory(DropDownList[] dropDownList, bool booladdSelectAsFirstElement)
+    {
+        DataTable dtData = ERIMS.DAL.clsLU_VOC_Category.SelectAll().Tables[0];
+        dtData.DefaultView.RowFilter = " Active = 'Y' ";
+        dtData = dtData.DefaultView.ToTable();
 
-    //    foreach (DropDownList ddlToFill in dropDownList)
-    //    {
-    //        ddlToFill.Items.Clear();
-    //        ddlToFill.DataTextField = "Category";
-    //        ddlToFill.DataValueField = "PK_LU_VOC_Category";
-    //        ddlToFill.DataSource = dtData;
-    //        ddlToFill.DataBind();
-    //        //check require to add "-- select --" at first item of dropdown.
-    //        if (booladdSelectAsFirstElement)
-    //        {
-    //            ddlToFill.Items.Insert(0, new ListItem(SELECT_STRING, "0"));
-    //        }
-    //    }
-    //}
+        foreach (DropDownList ddlToFill in dropDownList)
+        {
+            ddlToFill.Items.Clear();
+            ddlToFill.DataTextField = "Category";
+            ddlToFill.DataValueField = "PK_LU_VOC_Category";
+            ddlToFill.DataSource = dtData;
+            ddlToFill.DataBind();
+            //check require to add "-- select --" at first item of dropdown.
+            if (booladdSelectAsFirstElement)
+            {
+                ddlToFill.Items.Insert(0, new ListItem(SELECT_STRING, "0"));
+            }
+        }
+    }
 
     public static void FillYear(DropDownList[] ddlYear, bool booladdSelectAsFirstElement)
     {
