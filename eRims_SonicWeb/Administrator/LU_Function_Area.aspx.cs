f823382a498114c8f16fb458f2773c67720fc30a
birva.s <birva.s@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2016-01-04 09:48:15 +0000

    3474
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@72969 8092c8c6-7ab6-f546-91b1-2980cf571e14

---
+++ eRims_SonicWeb/Administrator/LU_Function_Area.aspx.cs
@@ -0,0 +1,178 @@
+﻿using ERIMS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Administrator_LU_Function_Area : clsBasePage
+{
+    #region "Properties"
+
+    /// <summary>
+    /// Denotes the Primary Key
+    /// </summary>
+    public decimal _PK_LU_Function_Area
+    {
+        get
+        {
+            return clsGeneral.GetInt(ViewState["PK_LU_Function_Area"]);
+        }
+        set { ViewState["PK_LU_Function_Area"] = value; }
+    }
+
+    #endregion
+
+    #region "Page Event"
+
+    /// <summary>
+    /// Page Load Event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //check Is Page Post Back
+        if (!IsPostBack)
+        {
+            //Bind Grid Function
+            BindGrid();
+        }
+    }
+
+    #endregion
+
+    #region "Event"
+
+    /// <summary>
+    /// Add new button link event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void lnkAddNew_Click(object sender, EventArgs e)
+    {
+        btnAdd.Text = "Add";
+        _PK_LU_Function_Area = 0;
+        trStatusAdd.Style.Add("display", "block");
+        lnkCancel.Style.Add("display", "inline");
+        txtDescription.Text = "";
+        ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtDescription);
+    }
+
+    /// <summary>
+    /// Cancel button click event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void lnkCancel_Click(object sender, EventArgs e)
+    {
+        trStatusAdd.Style.Add("display", "none");
+        lnkCancel.Style.Add("display", "none");
+        txtDescription.Text = "";
+    }
+
+    /// <summary>
+    /// Add new button to save data
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        decimal _retVal;
+        clsLU_Function_Area objLU_Function_Area = new clsLU_Function_Area();
+        objLU_Function_Area.Description = txtDescription.Text.Trim();
+        objLU_Function_Area.PK_LU_Function_Area = _PK_LU_Function_Area;
+
+
+        if (_PK_LU_Function_Area > 0)
+        {
+            _retVal = objLU_Function_Area.Update();
+            // Used to Check Duplicate Media ID?
+            if (_retVal == -2)
+            {
+                lblError.Text = "The Description that you are trying to add already exists in the Function Area table.";
+                ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtDescription);
+                return;
+            }
+            btnAdd.Text = "Add";
+        }
+        else
+        {
+            _retVal = objLU_Function_Area.Insert();
+            // Used to Check Duplicate Media ID?
+            if (_retVal == -2)
+            {
+                lblError.Text = "The Description that you are trying to add already exists in the Function Area table.";
+                ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtDescription);
+                return;
+            }
+            _PK_LU_Function_Area = _retVal;
+        }
+        //claer Control
+        ClearControls();
+        //Bind Grid Function
+        BindGrid();
+        //Cancel CLick
+        lnkCancel_Click(null, null);
+    }
+
+    #endregion
+
+    #region "Grid Event"
+    /// <summary>
+    /// Page Index Change Event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void gvLU_Function_Area_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvLU_Function_Area.PageIndex = e.NewPageIndex; //Page new index call
+        BindGrid();
+    }
+    /// <summary>
+    /// Row Command Event
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void gvLU_Function_Area_RowCommand(object sender, GridViewCommandEventArgs e)
+    {
+        if (e.CommandName == "EditRecord")
+        {
+            _PK_LU_Function_Area = Convert.ToDecimal(e.CommandArgument);
+            // show and hide Add-edit row
+            trStatusAdd.Style.Add("display", "block");
+            lnkCancel.Style.Add("display", "inline");
+            btnAdd.Text = "Update";
+            // get record from database
+            clsLU_Function_Area objFacilitySpec = new clsLU_Function_Area(_PK_LU_Function_Area);
+            txtDescription.Text = objFacilitySpec.Description.ToString();
+            ((ScriptManager)this.Master.FindControl("scMain")).SetFocus(txtDescription);
+        }
+    }
+
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Bind Facility Specification Section Grid
+    /// </summary>
+    private void BindGrid()
+    {
+        //Get Record into Dataset
+        DataSet dsFunctionArea = clsLU_Function_Area.SelectAll();
+        //Apply Dataset to Grid
+        gvLU_Function_Area.DataSource = dsFunctionArea;
+        gvLU_Function_Area.DataBind();
+    }
+
+    /// Used to Claer the controls
+    /// </summary>
+    private void ClearControls()
+    {
+        //clear control
+        _PK_LU_Function_Area = 0;
+    }
+
+    #endregion
+}
---
+++ eRims_SonicWeb/App_Code/SONIC_DAL/clsLU_Function_Area.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Data;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+
+namespace  ERIMS.DAL
+{
+	/// <summary>
+	/// Data access class for LU_Function_Area table.
+	/// </summary>
+	public sealed class clsLU_Function_Area
+	{
+		#region Private variables used to hold the property values
+
+		private decimal? _PK_LU_Function_Area;
+		private string _Description;
+
+		#endregion
+
+		#region Public Property
+
+		/// <summary>
+		/// Gets or sets the PK_LU_Function_Area value.
+		/// </summary>
+		public decimal? PK_LU_Function_Area
+		{
+			get { return _PK_LU_Function_Area; }
+			set { _PK_LU_Function_Area = value; }
+		}
+
+		/// <summary>
+		/// Gets or sets the Description value.
+		/// </summary>
+		public string Description
+		{
+			get { return _Description; }
+			set { _Description = value; }
+		}
+
+
+		#endregion
+
+		#region Default Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the clsLU_Function_Area class with default value.
+		/// </summary>
+		public clsLU_Function_Area() 
+		{
+
+
+		}
+
+		#endregion
+
+		#region Primary Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the clsLU_Function_Area class based on Primary Key.
+		/// </summary>
+		public clsLU_Function_Area(decimal pK_LU_Function_Area) 
+		{
+			DataTable dtLU_Function_Area = SelectByPK(pK_LU_Function_Area).Tables[0];
+
+			if (dtLU_Function_Area.Rows.Count == 1)
+			{
+				 SetValue(dtLU_Function_Area.Rows[0]);
+
+			}
+
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the clsLU_Function_Area class based on Datarow passed.
+		/// </summary>
+		private void SetValue (DataRow drLU_Function_Area) 
+		{
+				if (drLU_Function_Area["PK_LU_Function_Area"] == DBNull.Value)
+					this._PK_LU_Function_Area = null;
+				else
+					this._PK_LU_Function_Area = (decimal?)drLU_Function_Area["PK_LU_Function_Area"];
+
+				if (drLU_Function_Area["Description"] == DBNull.Value)
+					this._Description = null;
+				else
+					this._Description = (string)drLU_Function_Area["Description"];
+
+
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Inserts a record into the LU_Function_Area table.
+		/// </summary>
+		/// <returns></returns>
+		public int Insert()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Function_AreaInsert");
+
+			
+			if (string.IsNullOrEmpty(this._Description))
+				db.AddInParameter(dbCommand, "Description", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Description", DbType.String, this._Description);
+
+            int returnValue = Convert.ToInt32(db.ExecuteScalar(dbCommand));
+
+            return returnValue;
+		}
+
+		/// <summary>
+		/// Selects a single record from the LU_Function_Area table by a primary key.
+		/// </summary>
+		/// <returns>DataSet</returns>
+		private DataSet SelectByPK(decimal pK_LU_Function_Area)
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Function_AreaSelectByPK");
+
+			db.AddInParameter(dbCommand, "PK_LU_Function_Area", DbType.Decimal, pK_LU_Function_Area);
+
+			return db.ExecuteDataSet(dbCommand);
+		}
+
+		/// <summary>
+		/// Selects all records from the LU_Function_Area table.
+		/// </summary>
+		/// <returns>DataSet</returns>
+		public static DataSet SelectAll()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Function_AreaSelectAll");
+
+			return db.ExecuteDataSet(dbCommand);
+		}
+
+		/// <summary>
+		/// Updates a record in the LU_Function_Area table.
+		/// </summary>
+        public decimal Update()
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Function_AreaUpdate");
+
+			db.AddInParameter(dbCommand, "PK_LU_Function_Area", DbType.Decimal, this._PK_LU_Function_Area);
+			
+			if (string.IsNullOrEmpty(this._Description))
+				db.AddInParameter(dbCommand, "Description", DbType.String, DBNull.Value);
+			else
+				db.AddInParameter(dbCommand, "Description", DbType.String, this._Description);
+
+            return (Convert.ToDecimal(db.ExecuteScalar(dbCommand)));
+		}
+
+		/// <summary>
+		/// Deletes a record from the LU_Function_Area table by a composite primary key.
+		/// </summary>
+		public static void DeleteByPK(decimal pK_LU_Function_Area)
+		{
+			Database db = DatabaseFactory.CreateDatabase();
+			DbCommand dbCommand = db.GetStoredProcCommand("LU_Function_AreaDeleteByPK");
+
+			db.AddInParameter(dbCommand, "PK_LU_Function_Area", DbType.Decimal, pK_LU_Function_Area);
+
+			db.ExecuteNonQuery(dbCommand);
+		}
+	}
+}
