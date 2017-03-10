5363f24480ce563cb1820345f3d737adbbd9808e
mrunal.p <mrunal.p@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2017-03-10 11:20:26 +0000

    3902
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@92078 8092c8c6-7ab6-f546-91b1-2980cf571e14

--- eRims_SonicWeb/SONIC/ClaimInfo/Reconciliation_Report.aspx.cs
+++ eRims_SonicWeb/SONIC/ClaimInfo/Reconciliation_Report.aspx.cs
@@ -6,6 +6,8 @@ using System.Web.UI.WebControls;
 using System.Data;
 using System.Text;
 using ERIMS.DAL;
+using System.IO;
+using OfficeOpenXml;
 
 public partial class Claim_Reconciliation_Report : clsBasePage
 {
@@ -31,7 +33,49 @@ public partial class Claim_Reconciliation_Report : clsBasePage
 
                 if (ds != null && ds.Tables.Count > 0)
                 {
-                    ExportToExcel(ds);                    
+                    string strReconciliationReport = ExportToExcel(ds);
+
+                    //int trantablecount = ds.Tables.Count - 1; //Last table
+
+                    string strTransactionReport = string.Empty;
+                    if (ds.Tables.Count > 4 && ds.Tables[4].Rows.Count > 0)
+                    {
+                        strTransactionReport = GetTransactionReport(ds.Tables[4], "Transaction_Report");
+                    }
+
+                    string[] strAttachments = new string[2];
+
+                    strAttachments[0] = strReconciliationReport;
+                    if (strTransactionReport.Length > 0)
+                    {
+                        strAttachments[1] = strTransactionReport;
+                    }
+                    string strzipDir = AppConfig.DocumentsPath + "\\Reconciliation";
+
+                    clsGeneral.SetZipDirectory(strAttachments, strzipDir);
+
+                    int TotalfileCount = Directory.GetFiles(strzipDir, "*.*", SearchOption.AllDirectories).Length;
+                    if (TotalfileCount == 1)
+                    {
+                        string[] filePaths = Directory.GetFiles(AppConfig.DocumentsPath + "\\Reconciliation");
+
+                        System.IO.FileInfo file = new System.IO.FileInfo(filePaths[0]);
+
+                        HttpContext.Current.Response.Clear();
+                        HttpContext.Current.Response.AddHeader("content-disposition", string.Format("attachment; filename={0}", file.Name));
+                        HttpContext.Current.Response.AddHeader("Content-Length", file.Length.ToString());
+                        HttpContext.Current.Response.ContentType = "application/octet-stream";//ReturnExtension(clsGeneral.GetExtension(objAttachment.Attachment_Name));
+                        HttpContext.Current.Response.TransmitFile(filePaths[0]);
+                        HttpContext.Current.Response.Flush();
+                        HttpContext.Current.Response.End();
+                    }
+                    else
+                    {
+                        clsGeneral.ConvertZIP(strzipDir);
+                        System.IO.FileInfo file = new System.IO.FileInfo(strzipDir + ".Zip");
+                        clsGeneral.DownloadZIP(AppConfig.DocumentsPath + "Reconciliation");
+                    }
+
                     ds = null;
                 }
             }
@@ -58,7 +102,7 @@ public partial class Claim_Reconciliation_Report : clsBasePage
         return false;
     }
     
-    private void ExportToExcel(DataSet ds)
+    private string ExportToExcel(DataSet ds)
     {
         string strXml = string.Empty;
         string strGeneralXml = string.Empty;
@@ -66,7 +110,12 @@ public partial class Claim_Reconciliation_Report : clsBasePage
 
         strXml = XmlFileForWorkSheet(ds);        
         string excelXml = string.Format(strGeneralXml, strXml);
-        DownloadFile(excelXml, "Reconciliation Report");
+
+        string strReconciliationReport = SaveTempFile(excelXml, "Reconciliation Report");
+
+        return strReconciliationReport;
+
+        //DownloadFile(excelXml, "Reconciliation Report");
     }
 
     private string XmlFileForWorkSheet(DataSet Ds)
@@ -202,7 +251,7 @@ public partial class Claim_Reconciliation_Report : clsBasePage
         strHTML.Append("</Row>");
 
 
-        for (int cnt = 1; cnt <= Ds.Tables.Count - 2; cnt++)
+        for (int cnt = 1; cnt <= Ds.Tables.Count - 3; cnt++)
         {
             for (int i = 0; i <= Ds.Tables[cnt].Rows.Count - 1; i++)
             {
@@ -242,5 +291,128 @@ public partial class Claim_Reconciliation_Report : clsBasePage
         //{
         //    throw e;
         //}        
+    }
+
+    private string SaveTempFile(string strHTML, string sectionName)
+    {
+        //try
+        //{
+
+        string strPath = string.Empty;
+        strPath = AppConfig.SitePath + @"temp\" + "Reconciliation_Report.xls";
+
+        StringBuilder sbRecord = new StringBuilder();
+        sbRecord.Append(strHTML);
+
+        if (!Directory.Exists(AppConfig.SitePath + @"temp\"))
+            Directory.CreateDirectory(AppConfig.SitePath + @"temp\");
+
+        File.SetAttributes(AppDomain.CurrentDomain.BaseDirectory, FileAttributes.Normal);
+        using (StreamWriter sw = File.CreateText(strPath))
+        {
+            sw.Write(sbRecord.ToString());
+            sbRecord = new StringBuilder(string.Empty);
+        }
+
+        return strPath;
+    }
+
+    public string GetTransactionReport(DataTable dt, string Filename)
+    {
+        MemoryStream ms = DataTableToExcelXlsx(dt, "Sheet1");
+
+        string strPath = string.Empty;
+        strPath = AppConfig.SitePath + @"temp\" + "Transaction_Report.xlsx";
+
+        if (!Directory.Exists(AppConfig.SitePath + @"temp\"))
+            Directory.CreateDirectory(AppConfig.SitePath + @"temp\");
+        
+        ms.Seek(0, SeekOrigin.Begin);
+        FileStream outStream = File.OpenWrite(strPath);
+        ms.WriteTo(outStream);
+        outStream.Flush();
+        outStream.Close();
+
+        return strPath;
+        //HttpContext.Current.Response.Clear();
+        //ms.WriteTo(HttpContext.Current.Response.OutputStream);
+        //HttpContext.Current.Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        //HttpContext.Current.Response.AddHeader("Content-Disposition", "attachment;filename=" + Filename);
+        //HttpContext.Current.Response.StatusCode = 200;
+        //HttpContext.Current.Response.End();
+    }
+
+    public static MemoryStream DataTableToExcelXlsx(DataTable table, string sheetName)
+    {
+        MemoryStream Result = new MemoryStream();
+        ExcelPackage pack = new ExcelPackage();
+        ExcelWorksheet ws = pack.Workbook.Worksheets.Add(sheetName);
+
+        ws.Cells[1, 1].Value = "Transaction Report";
+        ws.Cells[1, 1].Style.Font.Bold = true;
+        ws.Cells[2, 1].Value = "From Sedgwick Spreadsheet";
+        ws.Cells[2, 1].Style.Font.Bold = true;
+
+        int col = 1;
+        int row = 4;
+        foreach (DataColumn column in table.Columns)
+        {
+            ws.Cells[row, col].Value = column.ColumnName.ToString();
+            ws.Cells[row, col].Style.Font.Bold = true;
+            col++;
+            ws.Cells[row, col - 1].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+        }
+        col = 1;
+        row = 5;
+        foreach (DataRow rw in table.Rows)
+        {
+            foreach (DataColumn cl in table.Columns)
+            {
+                if (rw[cl.ColumnName] != DBNull.Value)
+                {
+                    double num;
+                    if (double.TryParse(rw[cl.ColumnName].ToString(), out num))
+                    {
+                        ws.Cells[row, col].Value = Convert.ToDecimal(rw[cl.ColumnName]);
+                    }
+                    else
+                        ws.Cells[row, col].Value = rw[cl.ColumnName].ToString();
+
+                    if (cl.ColumnName.ToString().ToLower() == "claim number" && rw[0].ToString().ToLower() == "zzz total - match")
+                    {
+                        ws.Cells[row, col].Style.Font.Bold = true;
+                        ws.Cells[row, col].Value = ws.Cells[row, col].Value.ToString().Replace("zzz ", "");
+                        ws.Cells[row, 2].Style.Font.Bold = true;
+                        ws.Cells[row, 3].Style.Font.Bold = true;
+                    }
+
+                    if (cl.ColumnName.ToString().ToLower() == "claim number" && rw[0].ToString().ToLower() == "zzzz total - no match")
+                    {
+                        ws.Cells[row, col].Style.Font.Bold = true;
+                        ws.Cells[row, col].Value = ws.Cells[row, col].Value.ToString().Replace("zzzz ", "");
+                        ws.Cells[row, 2].Style.Font.Bold = true;
+                        ws.Cells[row, 3].Style.Font.Bold = true;
+                    } 
+                    
+                    if (cl.ColumnName.ToString().ToLower() == "transaction amount")
+                    {
+                        ws.Cells[row, col].Style.Numberformat.Format = "0.00";
+                    }
+                }
+                
+                col++;
+                ws.Cells[row, col - 1].Style.Border.BorderAround(OfficeOpenXml.Style.ExcelBorderStyle.Thin);
+            }
+            row++;
+            col = 1;
+        }
+
+        ws.Cells.Style.Font.Name = "Arial";
+        ws.Cells.AutoFitColumns();
+        ws.Column(1).Width = 30;
+        ws.Cells.Style.Font.Size = 10;
+
+        pack.SaveAs(Result);
+        return Result;
     }
 }
