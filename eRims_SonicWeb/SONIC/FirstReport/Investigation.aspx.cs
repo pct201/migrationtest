128c0bbec0a8c26f41d673add64bd4587932e02b
harshil.k <harshil.k@8092c8c6-7ab6-f546-91b1-2980cf571e14>
2020-01-16 12:22:38 +0000

    Story 229
    git-svn-id: https://192.168.0.7/svn/team11/eRIMS_SONIC_New/trunk/eRIMS_Sonic@134576 8092c8c6-7ab6-f546-91b1-2980cf571e14

--- eRims_SonicWeb/SONIC/FirstReport/Investigation.aspx.cs
+++ eRims_SonicWeb/SONIC/FirstReport/Investigation.aspx.cs
@@ -1734,9 +1734,12 @@ public partial class Exposures_Investigation : clsBasePage
         else
         {
             DataTable dtClaimInfo = WC_ClaimInfo.Select_Claim_Number(Convert.ToInt32(FK_WC_FR), "WC");
-            DataSet dtClaimDetail = WC_ClaimInfo.SelectByPK(Convert.ToInt32(Convert.ToInt32(dtClaimInfo.Rows[0]["ClaimID"])));
-            if(dtClaimDetail.Tables[0].Rows[0]["Date_Return_To_Work"] != null && dtClaimDetail.Tables[0].Rows[0]["Date_Return_To_Work"].ToString() != string.Empty)
-                txtReturnToWorkDate.Text = clsGeneral.FormatDateToDisplay(Convert.ToDateTime(dtClaimDetail.Tables[0].Rows[0]["Date_Return_To_Work"]));
+            if (dtClaimInfo.Rows.Count > 0)
+            {
+                DataSet dtClaimDetail = WC_ClaimInfo.SelectByPK(Convert.ToInt32(Convert.ToInt32(dtClaimInfo.Rows[0]["ClaimID"])));
+                if (dtClaimDetail.Tables[0].Rows[0]["Date_Return_To_Work"] != null && dtClaimDetail.Tables[0].Rows[0]["Date_Return_To_Work"].ToString() != string.Empty)
+                    txtReturnToWorkDate.Text = clsGeneral.FormatDateToDisplay(Convert.ToDateTime(dtClaimDetail.Tables[0].Rows[0]["Date_Return_To_Work"]));
+            }
         }
 
         #endregion
